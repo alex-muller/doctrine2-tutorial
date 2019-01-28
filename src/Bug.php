8d95ba45fabf940e4bd4d8bdc8e83b0c1f2d0b1e
Alex Muller <mullerbox@gmail.com>
2019-01-28 17:17:16 +0200

    add relations

---
+++ src/Bug.php
@@ -0,0 +1,128 @@
+<?php
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * @Entity
+ * @Table(name="bugs")
+ */
+class Bug
+{
+
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $description;
+    /**
+     * @Column(type="datetime")
+     * @var DateTime
+     */
+    protected $created;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $status;
+    /**
+     * @ManyToMany(targetEntity="Product")
+     */
+    protected $products;
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="assignedBugs")
+     */
+    protected $engineer;
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="reportedBugs")
+     */
+    protected $reporter;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function setEngineer(User $engineer)
+    {
+        $engineer->assignedToBug($this);
+        $this->engineer = $engineer;
+    }
+
+    public function setReporter(User $reporter)
+    {
+        $reporter->addReportedBug($this);
+        $this->reporter = $reporter;
+    }
+
+    public function assignToProduct(Product $product)
+    {
+        $this->products[] = $product;
+    }
+    /**
+     * @return ArrayCollection
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param DateTime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
---
+++ src/User.php
@@ -0,0 +1,71 @@
+<?php
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * @Entity
+ * @Table(name="users")
+ */
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="reporter")
+     * @var Bug[]
+     */
+    protected $reportedBugs;
+
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="engineer")
+     * @var Bug[]
+     */
+    protected $assignedBugs;
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
+    public function addReportedBug(Bug $bug)
+    {
+        $this->reportedBugs[] = $bug;
+    }
+
+    public function assignedToBug(Bug $bug)
+    {
+        $this->assignedBugs[] = $bug;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
